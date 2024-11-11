df25c95100983b0e5a6e59c2127c0bae5f455ae2
Flxw <lxcalflxw@gmail.com>
2024-11-11 18:10:54 +0100

    chore(c): formatting

--- code/sem1/programmieren-in-c/400-control-flow/401-switch_case.c
+++ code/sem1/programmieren-in-c/400-control-flow/401-switch_case.c
@@ -1,11 +1,18 @@
-// Switch case: Order your burger...
-// TO DO 1: Replace the if-statements with switch case and add a default case
-// for invalid input TO DO 2: Add support for extra-large and fish burgers TO DO
-// 3: Extend the program such that is executed in a loop, which can be aborted
-// by entering 'q' as size TO DO 4: If there is a much longer list of options:
-// How can long if-else-chains / switch-case-statements be avoided? TO DO 5:
-// Optional: Any further ideas for additional sizes and options?
-
+/* Switch case: Order your burger...
+ * TO DO 1: Replace the if-statements with switch case and add a default case
+ * for invalid input
+ *
+ * TO DO 2: Add support for extra-large and fish burgers
+ *
+ * TO DO 3: Extend the program such that is executed in a loop, which can be
+ * aborted by entering 'q' as size
+ *
+ * TO DO 4: If there is a much longer list of options:
+ * How can long if-else-chains / switch-case-statements be avoided?
+ * => Arrays with available options and a loop to check the input.
+ *
+ * TO DO 5: Optional: Any further ideas for additional sizes and options?
+ */
 #include <stdio.h>
 
 int main() {
@@ -17,13 +24,13 @@ int main() {
   while (1) {
     printf(
         "Which size you want your burger (s=small, m=medium, l=large, "
-        "x=extra-large)? Type 'q' to quit the ordering process.");
+        "x=extra-large)? Type 'q' to quit the ordering process.\n");
     scanf(" %c", &size);
     if ((size | 32) == 'q') {
       break;
     }
 
-    printf("Any specials (r=regular: beef, v=veggie, c=cheese)?");
+    printf("Any specials (r=regular: beef, v=veggie, c=cheese)?\n");
     scanf(" %c", &specials);
 
     switch (size) {
@@ -71,7 +78,7 @@ int main() {
       }
     }
     printf("\\______/\n");
-    printf("\n\n%.2f Euro - Enjoy!", price);
+    printf("\n\n%.2f Euro - Enjoy!\n", price);
   }
 
   return 0;
